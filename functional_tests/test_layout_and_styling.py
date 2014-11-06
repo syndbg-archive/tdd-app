e19079ff5c3df5498f8ed709693bd84b8ba350f5
syndg <syndbe@gmail.com>
2014-11-07 01:14:26 +0200

    Fix functional tests capturing a non-existing id

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -9,7 +9,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
-        inputbox = self.get_item_input_idebox()
+        inputbox = self.get_item_input_box()
         # self.assertAlmostEqual(
         #     inputbox.location['x'] + inputbox.size['width'] / 2,
         #     512,
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,6 @@ class ItemValidationTest(FunctionalTest):
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
-        self.check_for_row_in_list_table('1: Buy wellies')
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
@@ -41,5 +40,5 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Buy wellies\n')
 
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_id('.has-error')
+        error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, 'You\'ve already got this in your list')
