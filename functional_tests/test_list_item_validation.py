c12167168351f22c5d8c0c56bc696cb81b93e4c3
syndg <syndbe@gmail.com>
2014-11-09 02:09:10 +0200

    Added first JS functional test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -42,3 +42,14 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, 'You\'ve already got this in your list')
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertTrue(error.is_displayed())
+
+        # and if we start writing anew
+        self.gget_item_input_box().send_keys('a')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertFalse(error.is_displayed())
