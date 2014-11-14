d2dd7dabf82bffb1ba2452fb57c28c5e011bc8b6
syndg <syndbe@gmail.com>
2014-11-14 20:54:36 +0200

    Updated list.js.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,8 @@
-$('input').on('keypress', function () {
-    $('.has-error').hide();
-});
-$('input').on('click', function () {
-    $('.has-error').hide();
+$(document).ready(function () {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
+    $('input').on('click', function () {
+        $('.has-error').hide();
+    });
 });
