03c3b8a0c4a8d8ebac03225c0d186aa64bdea181
syndg <syndbe@gmail.com>
2014-11-14 19:16:14 +0200

    Added basic jQuery functionality

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -11,7 +14,7 @@ class ItemValidationTest(FunctionalTest):
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works
@@ -24,7 +27,7 @@ class ItemValidationTest(FunctionalTest):
         # She receives a similar warning on the list page
 
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
@@ -40,16 +43,16 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Buy wellies\n')
 
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, 'You\'ve already got this in your list')
 
     def test_error_messages_are_cleared_on_input(self):
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertTrue(error.is_displayed())
 
         # and if we start writing anew
-        self.gget_item_input_box().send_keys('a')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        self.get_item_input_box().send_keys('a')
+        error = self.get_error_element()
         self.assertFalse(error.is_displayed())
---
+++ lists/static/list.js
@@ -0,0 +1,6 @@
+$('input').on('keypress', function () {
+    $('.has-error').hide();
+});
+$('input').on('click', function () {
+    $('.has-error').hide();
+});
